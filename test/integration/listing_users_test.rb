8da494c8243b9301006dd06a27eeb49acc0b6c23
Deutronomium <patrick.engelkes@gmail.com>
2014-10-09 16:20:37 +0200

    Add users listing

---
+++ test/integration/listing_users_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ListingUsersTest < ActionDispatch::IntegrationTest
+  setup do
+    User.create!(userName: 'Deutro', firstName: 'Patrick', lastName: 'Engelkes')
+    User.create!(userName: 'Munni', firstName: 'Monique', lastName: 'Toepsch')
+  end
+
+  test 'listing users' do
+    get '/users'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    assert_equal User.count, JSON.parse(response.body).size
+  end
+end
