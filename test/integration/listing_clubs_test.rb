0b516fcc3912a849e6c592fcd1e20d0b60d7675f
Deutronomium <patrick.engelkes@gmail.com>
2014-10-10 15:41:55 +0200

    Fix inconsistency in user serializer

--- test/integration/listing_clubs_test.rb
+++ test/integration/listing_clubs_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class ListingClubsTest < ActionDispatch::IntegrationTest
+  setup do
+    @club = Club.create!(name: 'Glühwürmchen')
+
+    @club.users.create!(userName: 'Deutro', firstName: 'Patrick', lastName: 'Engelkes')
+    @club.users.create!(userName: 'Munni', firstName: 'Monique', lastName: 'Toepsch')
+  end
+
   test 'lists clubs' do
     get '/clubs'
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
+    assert_equal 1, json(response.body).size
   end
 end
