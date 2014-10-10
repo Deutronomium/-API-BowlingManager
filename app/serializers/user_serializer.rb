0b66d545305941c616186ff81553e2117931b78f
Deutronomium <patrick.engelkes@gmail.com>
2014-10-10 16:11:08 +0200

    Change belongs_to to has_one in user_serializer fixes inconsistent behaviour completely

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :userName, :firstName, :lastName, :mail, :street, :city, :club_id
+
+  has_one :club
+  embed :id, include: true
 end
