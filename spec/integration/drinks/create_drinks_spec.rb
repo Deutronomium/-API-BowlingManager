581b1b3d602c17d14c08af8760f6109877365d27
Deutro <patrick.engelkes@gmail.com>
2015-02-11 08:39:31 +0100

    Tests for invalid drinks and fines.

--- spec/integration/drinks/create_drinks_spec.rb
+++ spec/integration/drinks/create_drinks_spec.rb
@@ -27,4 +27,27 @@ describe 'creating drinks' do
       drink[:club_id].should eq(@club.id)
     end
   end
+
+  context 'with invalid data' do
+    it 'should respond with unprocessable entity' do
+      drink_name = 'Bier'
+      FactoryGirl.create(:drink, name: drink_name)
+      post '/drinks',
+           {
+               drink: {
+                   name: drink_name,
+                   price: 2.30,
+                   club_id: @club.id
+               }
+           }.to_json,
+           request_headers
+
+      response.status.should eq(422)
+
+      response.content_type.should eq(Mime::JSON)
+
+      error = json(response.body)[:error]
+      error[:message].should_not be nil
+    end
+  end
 end
--- spec/integration/fines/create_fines_spec.rb
+++ spec/integration/fines/create_fines_spec.rb
@@ -27,4 +27,27 @@ describe 'creating fine' do
       fine[:club_id].should eq(@club.id)
     end
   end
+
+  context 'with invalid data' do
+    it 'should respond with unprocessable entity' do
+      fine_name = 'Test'
+      FactoryGirl.create(:fine, name: fine_name)
+      post '/fines',
+           {
+               fine: {
+                   name: fine_name,
+                   amount: 5.00,
+                   club_id: @club.id
+               }
+           }.to_json,
+           request_headers
+
+      response.status.should eq(422)
+
+      response.content_type.should eq(Mime::JSON)
+
+      error = json(response.body)[:error]
+      error[:message].should_not be nil
+    end
+  end
 end
